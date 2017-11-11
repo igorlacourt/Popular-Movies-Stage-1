51fd68c34d8a647b36afde91d858c402a46d1fbd
igor lacourt <igorlacourt@gmail.com>
2017-11-11 15:20:17 -0200

    Removes API key.

--- app/src/main/java/com/udacity/lacourt/popularmoviesstage1/MoviesApiService.java
+++ app/src/main/java/com/udacity/lacourt/popularmoviesstage1/MoviesApiService.java
@@ -8,12 +8,12 @@ public interface MoviesApiService {
 //    @GET("/movie/popular")
 //    void getPopularMovies(Callback<Movie.MovieResult> cb);
 
-    @GET("movie/popular?api_key=fef98cf6bd829f53836bb7d92b02d6ef&language=en-US&page=1")
+    @GET("movie/popular?api_key=<<API_KEY>>&language=en-US&page=1")
     Call<PostResponse> getResponse();
 
-    @GET("movie/popular?api_key=fef98cf6bd829f53836bb7d92b02d6ef&language=en-US")
+    @GET("movie/popular?api_key=<<API_KEY>>&language=en-US")
     Call<PostResponse> getMostPopular(@Query("page") String page);
 
-    @GET("movie/top_rated?api_key=fef98cf6bd829f53836bb7d92b02d6ef&language=en-US")
+    @GET("movie/top_rated?api_key=<<API_KEY>>&language=en-US")
     Call<PostResponse> getTopRated(@Query("page") String page);
 }
